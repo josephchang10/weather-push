5394349f8e1b70017e373e560837eeda603b8519
张嘉夫 <zhangkuan523@icloud.com>
2016-12-23 16:48:24 +0800

    Initial Commit

---
+++ WeatherPush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherPush
+//
+//  Created by 张嘉夫 on 16/12/23.
+//  Copyright © 2016年 张嘉夫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
